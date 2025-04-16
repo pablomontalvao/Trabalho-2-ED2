3e0212bfff47accbc5f15e5773af621e19f0b0ab
GUSTAVO ANTUNES <159591602+stavoantunes@users.noreply.github.com>
2025-04-16 13:38:28 -0300

    Update codificacaoHuffman.h

--- include/codificacaoHuffman.h
+++ include/codificacaoHuffman.h
@@ -1,23 +1,27 @@
+#ifndef CODIFICACAO_HUFFMAN_H
+#define CODIFICACAO_HUFFMAN_H
+
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "listaEncadeadaOrdenada.h"  // Para o tipo No*
 
 void gerarCodigos(No *raiz, char *codigoAtual, int nivel, char* tabelaCodigos[256]) {
     if (!raiz)
         return;
 
-    // Verifica se é folha
     if (!raiz->esq && !raiz->dir) {
-        codigoAtual[nivel] = '\0';  // Finaliza a string
+        codigoAtual[nivel] = '\0';
         tabelaCodigos[raiz->caracter] = (char *)malloc(strlen(codigoAtual) + 1);
         strcpy(tabelaCodigos[raiz->caracter], codigoAtual);
         return;
     }
 
-    // Vai para a esquerda (bit 0)
     codigoAtual[nivel] = '0';
     gerarCodigos(raiz->esq, codigoAtual, nivel + 1, tabelaCodigos);
 
-    // Vai para a direita (bit 1)
     codigoAtual[nivel] = '1';
     gerarCodigos(raiz->dir, codigoAtual, nivel + 1, tabelaCodigos);
 }
+
+#endif
